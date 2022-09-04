9f6a20d0acdee54427e9107b1b859b42f3e49928
irenewlsr <84343714+irenewlsr@users.noreply.github.com>
2022-09-04 17:38:14 +0700

    Add files via upload

---
+++ HW6_61421037/Laundry.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6_61421037
+{
+    class Laundry
+    {
+        //Irene Wonlopsiri 6142103726
+
+        protected string consname;
+        protected string phonenum;
+        protected string roomnum;
+
+        public Laundry(string cname, string phone, string room)
+        {
+            consname = cname;
+            phonenum = phone;
+            roomnum = room;
+        }
+
+        public string Consname
+        {
+            get { return consname; }
+        }
+        public string Phonenum
+        {
+            get { return phonenum; }
+        }
+        public string Roomnum
+        {
+            get { return roomnum; }
+        }
+
+        public virtual void Useservice(int clothing, int blanket, int hanger)
+        { 
+            if (hanger <= clothing + blanket)
+            {
+                hanger = ((clothing + blanket) - hanger) * 10;
+                Console.WriteLine("You have to pay " + hanger + " baht for the cloth hanger");
+            }
+        }
+    }
+
+    class Package : Laundry
+    {
+        private int points;
+
+        public int Points
+        {
+            get { return points; }
+        }
+
+        public Package (string cname, string phone, string room): base (cname, phone, room)
+        {
+            
+        }
+
+        public void buypackage ()
+        {
+            int packc;
+            Console.WriteLine("Package\n1.20 points\n2.50 points\n3.100 points");
+            Console.WriteLine("Chose your package : ");
+            packc = Convert.ToInt32(Console.ReadLine());
+            if (packc == 1)
+            {
+                points = points + 20;
+
+                Console.WriteLine("You have " + points + " points left");
+            }
+            else if (packc == 2)
+            {
+                points = points + 50;
+
+                Console.WriteLine("You have " + points + " points left");
+            }
+            else if (packc == 3)
+            {
+                points = points + 100;
+
+                Console.WriteLine("You have " + points + " points left");
+            }
+            else
+            {
+                Console.WriteLine("Enter 1/2/3 only!");
+            }
+        }
+
+        public override void Useservice(int clothing, int blanket, int hanger)
+        {
+            base.Useservice(clothing, blanket, hanger);
+
+            int pointused;
+            int pointleft;
+
+            pointused = clothing + (blanket * 3);
+            pointleft = points - pointused;
+            Console.WriteLine("You have " + pointleft + " points left");
+        }
+    }
+
+
+    class Regular : Laundry
+    {
+
+        public Regular(string cname, string phone, string room) : base(cname, phone, room)
+        {
+
+        }
+
+        public override void Useservice(int clothing, int blanket, int hanger)
+        {
+            base.Useservice(clothing, blanket, hanger);
+
+            int pricec;
+            int priceb;
+            int totprice;
+
+            pricec = clothing * 15;
+            priceb = blanket * 45;
+            totprice = pricec + priceb;
+
+            Console.WriteLine("Laundry services price (Does not include hanger price) : " + totprice + " baht");
+        }
+    }
+}
---
+++ HW6_61421037/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HW6_61421037
+{
+    class Program
+    {
+        //Irene Wonlopsiri 6142103726
+        static void Main(string[] args)
+        {
+            int choice;
+            int i;
+            string cname, phone, room;
+            Console.WriteLine("Menu\n1.Add custumer\n2.Buy more package\n3.Use laundry services\n4.Exit");
+            Console.WriteLine("Enter your choice : ");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            ArrayList cons = new ArrayList();
+
+            while (choice != 4)
+            {
+                if (choice == 1)
+                {
+                    int yesno;
+
+                    Console.WriteLine("Enter consumer name : ");
+                    cname = Console.ReadLine();
+                    Console.WriteLine("Enter consumer phone number : ");
+                    phone = Console.ReadLine();
+                    Console.WriteLine("Enter consumer room number : ");
+                    room = Console.ReadLine();
+
+                    Console.WriteLine("Do you want to buy package?(1=yes/2=no) : ");
+                    yesno = Convert.ToInt32(Console.ReadLine());
+
+                    if (yesno == 1)
+                    {
+                        Package p = new Package(cname, phone, room);
+                        cons.Add(p);
+
+                        for (i = 0; i < cons.Count; i++)
+                            ((Package)cons[i]).buypackage();
+                    }
+
+                    else if (yesno == 2)
+                    {
+                        Regular r = new Regular(cname, phone, room);
+                        cons.Add(r);
+
+                        Console.WriteLine("Custumer added");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter 1/2 only!");
+                    }
+                }
+
+                else if (choice == 2)
+                {
+                    Console.WriteLine("Enter consumer name : ");
+                    cname = Console.ReadLine();
+                    for (i = 0; i < cons.Count; i++)
+                        if (((Package)cons[i]).Consname == cname)
+                        {
+                            ((Package)cons[i]).buypackage(); break;
+                        }
+                }
+
+                else
+                {
+                    int clothing, blanket, hanger;
+                    Console.WriteLine("Enter consumer name : ");
+                    cname = Console.ReadLine();
+                    for (i = 0; i < cons.Count; i++)
+                        if (((Laundry)cons[i]).Consname == cname)
+                        {
+                            Console.WriteLine("Enter number of shirts/pants/skirts : ");
+                            clothing = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter number of blanket : ");
+                            blanket = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter number of colth hangers : ");
+                            hanger = Convert.ToInt32(Console.ReadLine());
+
+                            ((Laundry)cons[i]).Useservice(clothing, blanket, hanger);
+                        }
+                }
+
+                Console.WriteLine("Menu\n1.Add custumer\n2.Buy more package\n3.Use laundry services\n4.Exit");
+                Console.WriteLine("Enter your choice : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+            }
+            
+
+        }
+    }
+}
